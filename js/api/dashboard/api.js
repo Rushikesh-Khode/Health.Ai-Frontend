14a34c2d87c4c98b286b080009e23362c7c3220c
rushikesh <rushikesh.khode@noovosoft.com>
2023-05-21 23:51:35 +0530

    Add warning before uploading image

--- js/api/dashboard/api.js
+++ js/api/dashboard/api.js
@@ -1,11 +1,14 @@
 
 async function upload() {
+    hideNotice()
+    showLoader()
     hideTable()
     const image = document.getElementById("file-upload").files[0]
     const size = (image.size / 1024 / 1024).toFixed(2);
 
     if (size > 1) {
         alert("File must be not more than size of 1 MB");
+        hideLoader()
         return;
     }
 
@@ -20,6 +23,7 @@ async function upload() {
                 alert(response["error"])
             } else {
                 showTable()
+                hideLoader()
                 const result = getMaxValueClass(response['prediction'])
                 const resultBox = document.getElementById("result")
                 const resultImage = document.getElementById("result_image")
@@ -30,13 +34,13 @@ async function upload() {
                 title.appendChild(titleText)
 
                 const pred = document.createElement("h2")
-                const predText = document.createTextNode(""+ (result[1] * 100).toFixed(2) + "%")
+                const predText = document.createTextNode("" + (result[1] * 100).toFixed(2) + "%")
                 pred.appendChild(predText)
 
                 var image = document.createElement("img");
                 image.setAttribute("src", base64Image);
-                image.setAttribute("width", 612)
-                image.setAttribute("height", 612)
+                image.setAttribute("width", 420)
+                image.setAttribute("height", 420)
 
                 resultBox.replaceChildren(...[title])
                 resultImage.replaceChildren(...[image])
@@ -69,12 +73,33 @@ function getMaxValueClass(result) {
     return [maxClass, maxValue]
 }
 
-function showTable(){
+function showTable() {
     const resultCard = document.getElementById("card")
     resultCard.style.display = "block"
 }
 
-function hideTable(){
+function hideTable() {
     const resultCard = document.getElementById("card")
     resultCard.style.display = "none"
+}
+
+function showLoader() {
+    const loader = document.getElementById("loader")
+    loader.style.display = "block"
+}
+
+function hideLoader() {
+    const loader = document.getElementById("loader")
+    loader.style.display = "none"
+}
+
+function showNotice() {
+    hideNotice()
+    const notice = document.getElementById("notice")
+    notice.style.display = "block"
+}
+
+function hideNotice() {
+    const notice = document.getElementById("notice")
+    notice.style.display = "none"
 }
